f36b3cb1df424135bb07648029944a1d7958b084
Nstampfli <nathan.stampfli@hetic.net>
2020-01-02 03:19:08 +0100

    Push All Game Rock Paper Scissors

---
+++ js/app.js
@@ -0,0 +1,119 @@
+const game = () => {
+  let pScore = 0;
+  let cScore = 0;
+
+  //Let's Play Start Game
+  const startGame = () => {
+    const playBtn = document.querySelector(".intro button");
+    const introScreen = document.querySelector(".intro");
+    const match = document.querySelector(".match");
+
+    playBtn.addEventListener("click", () => {
+      introScreen.classList.add("fadeOut");
+      match.classList.add("fadeIn");
+    });
+  };
+
+  //Play Match
+  const playMatch = () => {
+    const options = document.querySelectorAll(".options button");
+    const playerHand = document.querySelector(".player-hand");
+    const computerHand = document.querySelector(".computer-hand");
+    const hands = document.querySelectorAll(".hands img");
+
+    hands.forEach(hand => {
+      hand.addEventListener("animationend", function() {
+        this.style.animation = "";
+      });
+    });
+
+    //Computer Options with Array
+    const computerOptions = ["rock", "paper", "scissors"];
+
+    options.forEach(option => {
+      option.addEventListener("click", function() {
+        //Computer Choice with MathRandom
+        const computerNumber = Math.floor(Math.random() * 3);
+        const computerChoice = computerOptions[computerNumber];
+
+        setTimeout(() => {
+          //Call Function to compare the Hands
+          compareHands(this.textContent, computerChoice);
+          //Update Images with Delay
+          playerHand.src = `./img/${this.textContent}.png`;
+          computerHand.src = `./img/${computerChoice}.png`;
+        }, 2000);
+        //Animation Shake
+        playerHand.style.animation = "shakePlayer 2s ease";
+        computerHand.style.animation = "shakeComputer 2s ease";
+      });
+    });
+  };
+
+  //Update Score
+  const updateScore = () => {
+    const playerScore = document.querySelector(".player-score p");
+    const computerScore = document.querySelector(".computer-score p");
+    playerScore.textContent = pScore;
+    computerScore.textContent = cScore;
+  };
+  //CompareHands to known who win
+  const compareHands = (playerChoice, computerChoice) => {
+    //Update Text
+    const winner = document.querySelector(".winner");
+    //Checking for a tie
+    if (playerChoice === computerChoice) {
+      winner.textContent = "It is a tie";
+      return;
+    }
+    //Check for Rock
+    if (playerChoice === "rock") {
+      if (computerChoice === "scissors") {
+        winner.textContent = "Player Wins";
+        pScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = "Computer Wins";
+        cScore++;
+        updateScore();
+        return;
+      }
+    }
+    //Check for Paper
+    if (playerChoice === "paper") {
+      if (computerChoice === "rock") {
+        winner.textContent = "Player Wins";
+        cScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = "Computer Wins";
+        pScore++;
+        updateScore();
+        return;
+      }
+    }
+    //Check for Scissors
+    if (playerChoice === "scissors") {
+      if (computerChoice === "paper") {
+        winner.textContent = "Player Wins";
+        cScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = "Computer Wins";
+        pScore++;
+        updateScore();
+        return;
+      }
+    }
+  };
+
+  //Call Function to play (Let's Play + Game)
+  startGame();
+  playMatch();
+};
+
+//Call Main Function
+game();
